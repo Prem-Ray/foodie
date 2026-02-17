8acf642d5a2e3aa7d423b6217499b9326bae67a2
Prem-Ray <premanshuray981@gmail.com>
2026-02-17 20:50:59 +0530

    added login log out button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from 'react' ;
-import {StyledNavbar,Stylednavitems} from './Navbar.style'
+import React, { useState } from "react";
+import { StyledNavbar, Stylednavitems } from "./Navbar.style";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
 
+export const Title = () => {
+  return <h2>Foodie</h2>;
+};
 
+export const Navitems = () => {
+  return (
+    <Stylednavitems>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Cart</li>
+    </Stylednavitems>
+  );
+};
 
-export const Title = ()=>{
-    return (
-       <h2>Foodie</h2>
-    )
-}
+export const Navbar = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
+  return (
+    <StyledNavbar>
+      <Title />
+      <Navitems />
 
-export const Navitems = ()=>{
-    return (
-        <Stylednavitems>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Cart</li>
-        </Stylednavitems>
-    )
-}
-
-
-export const Navbar = ()=>{
-    return (
-       <StyledNavbar>
-        <Title/>
-        <Navitems/>
-       </StyledNavbar>
-    )
-}
+      <Button
+        variant="contained"
+        startIcon={isLoggedIn ? <LogoutIcon /> : <LoginIcon />}
+        onClick={() => setLoggedIn(!isLoggedIn)}
+        sx={{
+             width: "120px",
+          textTransform: "none",
+          borderRadius: "10px",
+        }}
+      >
+        {isLoggedIn ? "Logout" : "Login"}
+      </Button>
+    </StyledNavbar>
+  );
+};
