ab9108521946ad6d07c40a5c74a46e755af087d6
Prem-Ray <premanshuray981@gmail.com>
2026-02-18 22:15:18 +0530

    resturant's menu page added

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -9,6 +9,10 @@ import {
   Typography,
   Box,
   CircularProgress,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
 } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 
@@ -18,7 +22,7 @@ export const Restaurant = () => {
 
   useEffect(() => {
     getRestaurantInfo();
-  }, []);
+  }, [resId]);
 
   async function getRestaurantInfo() {
     try {
@@ -33,17 +37,6 @@ export const Restaurant = () => {
     }
   }
 
-  const restaurantName = restaurant?.data?.cards?.[0]?.card?.card?.text;
-
-  const mainRestaurantInfoCard = restaurant?.data?.cards?.find(
-    (cardWrapper) =>
-      cardWrapper?.card?.card?.["@type"] ===
-      "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-  );
-
-  const mainRestaurantInfo = mainRestaurantInfoCard?.card?.card?.info;
-  const imageId = mainRestaurantInfo?.cloudinaryImageId;
-
   if (!restaurant) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center", mt: 10 }}>
@@ -51,6 +44,28 @@ export const Restaurant = () => {
       </Box>
     );
   }
+
+  // Restaurant name
+  const restaurantName = restaurant?.data?.cards?.[0]?.card?.card?.text;
+
+  // Main restaurant info
+  const mainRestaurantInfoCard = restaurant?.data?.cards?.find(
+    (cardWrapper) =>
+      cardWrapper?.card?.card?.["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  );
+
+  const mainRestaurantInfo = mainRestaurantInfoCard?.card?.card?.info;
+
+  const imageId = mainRestaurantInfo?.cloudinaryImageId;
+
+  const menuItems =
+    restaurant?.data?.cards
+      ?.find((card) => card?.groupedCard)
+      ?.groupedCard?.cardGroupMap?.REGULAR?.cards?.flatMap(
+        (card) => card?.card?.card?.itemCards || [],
+      )
+      ?.map((item) => item?.card?.info) || [];
 
   return (
     <Container maxWidth="md" sx={{ mt: 5 }}>
@@ -99,6 +114,45 @@ export const Restaurant = () => {
             </Typography>
           </Grid>
         </Grid>
+
+        {/* ✅ Menu Section */}
+        <Box sx={{ mt: 5 }}>
+          <Typography variant="h5" gutterBottom>
+            Menu
+          </Typography>
+
+          {menuItems.length > 0 ? (
+            <List>
+              {menuItems.map((item) => (
+                <React.Fragment key={item?.id}>
+                  <ListItem>
+                    <ListItemText
+                      primary={item?.name}
+                      secondary={`₹${
+                        (item?.price || item?.defaultPrice || 0) / 100
+                      }`}
+                    />
+                    {/* Image Section */}
+                    <Grid item xs={12} md={5}>
+                      <CardMedia
+                        component="img"
+                        height="250"
+                        image={IMG_URL + item?.imageId}
+                        alt={restaurantName}
+                        sx={{ borderRadius: 2 }}
+                      />
+                    </Grid>
+                  </ListItem>
+                  <Divider />
+                </React.Fragment>
+              ))}
+            </List>
+          ) : (
+            <Typography variant="body2" color="text.secondary">
+              No menu items found.
+            </Typography>
+          )}
+        </Box>
       </Paper>
     </Container>
   );
