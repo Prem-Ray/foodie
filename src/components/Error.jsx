a8f0c1ac1cc49e5ed33556526fd00400ad1c5883
Prem-Ray <premanshuray981@gmail.com>
2026-03-01 21:28:25 +0530

    fix error condition

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,9 +7,11 @@ export const Error = () => {
     <Box display="flex" justifyContent="center" mt={4}>
       <Alert severity="error" sx={{ width: "100%", maxWidth: 500 }}>
         <AlertTitle>Error</AlertTitle>
-        <AlertTitle>
-          {error ?? error.status} : {error ?? error.statusText}
-        </AlertTitle>
+        {
+          error ? <AlertTitle>
+          {error.status} : {error.statusText}
+        </AlertTitle> : null 
+        }
         Something went wrong. Please try again.
       </Alert>
     </Box>
